f7c10820ca68595e8d4549ef6aaffdd7ae5834b7
Ankush Ladani <astarladani@gmail.com>
2022-06-16 21:26:36 +0530

    Assignment 1 Done

---
+++ Assignment_1.js
@@ -0,0 +1,42 @@
+// ? 1. Explain the use of JavaScript ( or What you can do using a JavaScript)
+// We can use JavaScript for
+// Android Apps
+// Web Apps
+// Blockchain Apps
+// Machine Learning
+// Backend Server
+// Frontend UI
+// API's
+
+// ? 2. What is the difference between client-side and server-side?
+// Client Side means the user side in which any app is running.
+// Server Side means where the server actually located and data is fetched from it.
+
+// ! There are many types of architecture too
+// Tier 1
+// Tier 2
+// Tier 3
+
+
+// ? 3.  What is Nodejs?
+// It is a javascript environment which runs on browser only.
+
+// ? 4. Explain Scope in JavaScript
+// Scope means that particular variable's availabily is how much?
+// Till what limits it can be accesed
+
+// Block scope
+// Function scope
+// Lexical scope
+
+// ? 5. JavaScript is asynchronous or synchronous.
+// Synchronous
+
+// ? 6. JavaScript is Single-threaded or Multi-threaded.
+// Single Threaded
+
+// ?  7. Explain DOM in your own word.
+// DOM -> Document Object Model
+// So basically our all elements are arranged in a tree-like structure.
+// And top of tree is document itself
+// So if we want to access any branch/child of tree we can use document for that.
