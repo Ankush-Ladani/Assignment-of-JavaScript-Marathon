fb5924fb4f76a2041eb584b41618499b56b678eb
Ankush Ladani <astarladani@gmail.com>
2022-06-17 21:04:29 +0530

    Assignment 2 Done

---
+++ Assignment_2.js
@@ -0,0 +1,27 @@
+// ? 1. What is lexical structure?
+// So lexical structure are the basics rules in programming language
+// which decides that how you should write code in particular language.
+
+// ? 2. What is Unicode?
+// From my point of view Unicode is mixture of string and number
+// For ex. "\uxxxx"
+// Sir has example of e dilde.
+
+// ? 3. Explain all the keyboards events present in the JavaScript with examples.
+// keyPress -> When ever any key is pressed this event will fire up.
+// keyUp -> When ever any key is release it will move from bottom to top and at this time this event will be fired up.
+// keyDown -> When ever any key is Pressed it will move from top to bottom and at this time this event will be fired up.
+// keyDown and keyPress are similar
+
+// ? 4. What are shorthand operators, explain with a suitable example?
+// Shorthand operators are the operators which are mix up of two operators.
+// For Ex. 
+// a = a + b  ===>  a += b
+// a = a - b  ===>  a -= b  and similar like this
+// There is one ternary operator which is like this (condition) ? (if condition is true) : (if condition is false)
+// There is one short circuit operator (condition) && (Runs only if condition is true)
+
+// ? 5. What is “use Strict” in JavaScript?
+// It is used for more strictness to our code
+// For ex. if strict mode is off we can define variable using only name
+// But if strict mode is on then we can define variable using particular keyword( let , const , var)
